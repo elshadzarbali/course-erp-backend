664f9a82d7b7789227f85f2104c306dce9bf18ed
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-03-22 01:31:24 +0400

    Adding validation to LoginPayload

--- src/main/java/com/mycompany/courseerpbackend/models/payload/auth/LoginPayload.java
+++ src/main/java/com/mycompany/courseerpbackend/models/payload/auth/LoginPayload.java
@@ -1,5 +1,6 @@
 package com.mycompany.courseerpbackend.models.payload.auth;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,9 +11,17 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginPayload {
 
-    // todo: validation
+    // TODO: (IT) My message can be message code which stores in database
+
+    @Email(message = "Email is not valid format")
+    @NotBlank(message = "Email can not be empty")
+    @Size(min = 5, max = 254, message = "Email should be between 5 and 254 characters")
     String email;
+
+    @NotBlank(message = "Password can not be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     String password;
+
     boolean rememberMe;
 
 }
