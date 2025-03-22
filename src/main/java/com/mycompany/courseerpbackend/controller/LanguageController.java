d224b2982c0010bab8b5af44a906e62c12f30c23
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-03-22 13:24:48 +0400

    refactor: return LanguageResponse instead of Language in /v1/languages/localizable

--- src/main/java/com/mycompany/courseerpbackend/controller/LanguageController.java
+++ src/main/java/com/mycompany/courseerpbackend/controller/LanguageController.java
@@ -4,6 +4,7 @@ import com.mycompany.courseerpbackend.models.base.BaseResponse;
 import com.mycompany.courseerpbackend.models.mappers.LanguageEntityMapper;
 import com.mycompany.courseerpbackend.models.mybatis.language.Language;
 import com.mycompany.courseerpbackend.models.payload.language.LanguagePayload;
+import com.mycompany.courseerpbackend.models.response.language.LanguageResponse;
 import com.mycompany.courseerpbackend.services.language.LanguageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -34,8 +35,12 @@ public class LanguageController {
     }
 
     @GetMapping("/localizable")
-    public BaseResponse<List<Language>> getAllLocalizableLanguages() {
-        return BaseResponse.success(languageService.getAllLocalizableLanguages());
+    public BaseResponse<List<LanguageResponse>> getAllLocalizableLanguages() {
+        return BaseResponse.success(
+                LanguageEntityMapper.INSTANCE.fromLanguageToLanguageResponse(
+                        languageService.getAllLocalizableLanguages()
+                )
+        );
     }
 
 }
